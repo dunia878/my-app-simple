fa6dacef873d6e31a4cffb8cc5d3bc53e2abc388
gengjz <gengjz@ccic.com>
2015-04-30 09:51:56 +0800

    Signed-off-by: gengjz <gengjz@ccic.com>

---
+++ my-app-simple/src/main/java/com/geng/maven/my_app_simple/hello/MessagePrinter.java
@@ -0,0 +1,18 @@
+package com.geng.maven.my_app_simple.hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessagePrinter {
+	final private MessageService service;
+
+	@Autowired
+	public MessagePrinter(MessageService service) {
+		this.service = service;
+	}
+
+	public void printMessage() {
+		System.out.println(this.service.getMessage());
+	}
+}
---
+++ my-app-simple/src/main/java/com/geng/maven/my_app_simple/hello/MessageService.java
@@ -0,0 +1,5 @@
+package com.geng.maven.my_app_simple.hello;
+
+public interface MessageService {
+	String getMessage();
+}
